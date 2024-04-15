cfcab8b0edea7694da33ca03f808ef92cd9ea662
hasnainatique <hasnainatique786@gmail.com>
2024-04-15 16:08:55 +0500

    live

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-ED5NLLYG.mjs";import"./chunk-EM6GFXB2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/components/template1/template1.component.ts
+++ src/components/template1/template1.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-template1',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './template1.component.html',
   styleUrl: './template1.component.css'
 })
 export class Template1Component {
-
+  showMobileMenu: boolean = false;
+  constructor(private renderer: Renderer2) { }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.onResize1(event);
+  }
+  showMenu(show: boolean) {
+    this.showMobileMenu = show
+  }
+  onResize1(event: any) {
+    if (window.innerWidth < 768) {
+      this.showMobileMenu = true;
+    }
+  }
 }
