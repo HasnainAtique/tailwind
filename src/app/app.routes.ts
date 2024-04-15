c53f5b236fbae9af8082a148ecb595650ff9bbbc
hasnainatique <hasnainatique786@gmail.com>
2024-04-15 20:41:26 +0500

    CHANGINGS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PracticeComponent } from '../components/practice/practice.component';
 import { Template1Component } from '../components/template1/template1.component';
+import { Temp2Component } from '../components/temp2/temp2.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'temp1',
         component: Template1Component
+    },
+    {
+        path: 'temp2',
+        component: Temp2Component
     },
 ];
---
+++ src/components/temp2/temp2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temp2',
+  standalone: true,
+  imports: [],
+  templateUrl: './temp2.component.html',
+  styleUrl: './temp2.component.css'
+})
+export class Temp2Component {
+
+}
