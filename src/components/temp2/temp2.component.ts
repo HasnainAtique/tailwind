47364d522e1fd81fa75cf543717c227eb4bd7ec0
HasnainAtique <hasnainatique@gmail.com>
2024-04-19 10:42:11 -0700

    changings

--- src/components/temp2/temp2.component.ts
+++ src/components/temp2/temp2.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-temp2',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './temp2.component.html',
   styleUrl: './temp2.component.css'
 })
 export class Temp2Component {
-
+  showMobileMenu: boolean = false;
+  constructor(private renderer: Renderer2) { }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.onResize1(event);
+  }
+  showMenu(show: boolean) {
+    this.showMobileMenu = show
+  }
+  onResize1(event: any) {
+    if (window.innerWidth < 768) {
+      this.showMobileMenu = true;
+    }
+  }
 }
